cc6188ac0c26cd8263f9f68adb33db9fd895f957
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-10 17:27:29 +0300

    DOne

---
+++ 0x01-challenge/status_server/api/v1/views/app_views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify
+
+app_views = Blueprint('app_views', __name__)
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
